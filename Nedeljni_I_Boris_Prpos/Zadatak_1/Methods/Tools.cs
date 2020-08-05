fc6f0d14162c507e9ab14237a8cb6502f2a488d9
BorisP1996 <prposboris13@hotmail.com>
2020-08-05 23:34:42 +0200

    admin created and validated

---
+++ Nedeljni_I_Boris_Prpos/Zadatak_1/Methods/Tools.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Zadatak_1.Model;
+
+namespace Zadatak_1.Methods
+{
+    class Tools
+    {
+        /// <summary>
+        /// Method gets all admins to list
+        /// </summary>
+        /// <returns></returns>
+        public List<vwAdmin> GetAdmins()
+        {
+            try
+            {
+                using (Entity context = new Entity())
+                {
+                    List<vwAdmin> adminList = new List<vwAdmin>();
+                    adminList = context.vwAdmins.ToList();
+                    return adminList;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                return null;
+            }
+        }
+        public List<tblAdminType> GetAdminsList()
+        {
+            try
+            {
+                using (Entity context = new Entity())
+                {
+                    List<tblAdminType> adminList = new List<tblAdminType>();
+                    adminList = context.tblAdminTypes.ToList();
+                    return adminList;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+                return null;
+            }
+        }
+        public List<tblGender> GetGenders()
+        {
+            try
+            {
+                using (Entity context = new Entity())
+                {
+                    List<tblGender> genderList = new List<tblGender>();
+                    genderList = context.tblGenders.ToList();
+                    return genderList;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+                return null;
+            }
+        }
+
+        public List<tblMarried> GetMarried()
+        {
+            try
+            {
+                using (Entity context = new Entity())
+                {
+                    List<tblMarried> marriedList = new List<tblMarried>();
+                    marriedList = context.tblMarrieds.ToList();
+                    return marriedList;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+                return null;
+            }
+        }
+        public bool ValiationJMBG(string input)
+        {
+            
+            char[] array = input.ToCharArray();
+
+            int counter = 0;
+            //there must be 13 characaters
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (Char.IsDigit(array[i]))
+                {
+                    counter++;
+                }
+            }
+            if (counter == 13)
+            {
+                int[] daysInMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+                char[] inputToArray = input.ToCharArray(0, 13); // string to char array
+
+                // checking year of birth   
+                char[] yearOfBirth = input.ToCharArray(4, 3); // extract digits that represent year
+                int helpYear = 100 * (Convert.ToInt32(yearOfBirth[0] - '0')) +
+                                 10 * (Convert.ToInt32(yearOfBirth[1] - '0')) +
+                                       Convert.ToInt32(yearOfBirth[2] - '0'); // create year of birth
+
+                if (yearOfBirth[0] == '0') // born in XXI century ...
+                    helpYear += 2000;
+                else
+                    helpYear += 1000; // born in XX century
+
+                if (helpYear < 1900) // entered year smaller than 1900
+                {
+                    MessageBox.Show("Entered year smaller than 1900");
+                    return false;
+                }
+                else
+                {
+                    if (helpYear > DateTime.Now.Year) // entered year bigger than current year !
+                    {
+                        MessageBox.Show("Entered year bigger than current year");
+                        return false;
+                    }
+                }
+
+                // checking month of birth
+                char[] monthOfBirth = input.ToCharArray(2, 2); // extract digits that represent month
+                int helpMonth = 10 * (Convert.ToInt32(monthOfBirth[0] - '0')) +
+                                      Convert.ToInt32(monthOfBirth[1] - '0');
+                if (helpMonth > 12 || helpMonth < 1) // mont must be <= 12 and > 0 
+                {
+                    MessageBox.Show("Wrong month of birth (third and fourth digit)");
+                    return false;
+                }
+
+                // check if february has 29 days
+                if (((helpYear % 4) == 0) && (((helpYear % 100) != 0) || ((helpYear % 400) == 0))) // prestupna year
+                {
+                    daysInMonth[1] = 29; // correction for days in february
+                }
+
+                // check if month and days are compatible
+                char[] dayOfBirth = input.ToCharArray(0, 2);
+                int helpDay = 10 * (Convert.ToInt32(dayOfBirth[0] - '0')) +
+                                   Convert.ToInt32(dayOfBirth[1] - '0');
+
+                if (helpDay > daysInMonth[helpMonth - 1] || helpDay < 1)
+                {
+                    MessageBox.Show("Wrong day of birth (first and second digit)");
+                    return false;
+                }
+
+            }
+            //first and thirs number must be correct
+            if (counter == 13 && Convert.ToInt32(array[0].ToString()) < 4 && Convert.ToInt32(array[2].ToString()) < 2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool CheckCredentials(string username,string pasword,string jmbg)
+        {
+            try
+            {
+                using (Entity context = new Entity())
+                {
+                    List<tblUser> userList = new List<tblUser>();
+                    userList = context.tblUsers.ToList();
+                    List<string> usernameList = new List<string>();
+                    List<string> paswordList = new List<string>();
+                    List<string> jmbgList = new List<string>();
+
+                    foreach (tblUser item in userList)
+                    {
+                        usernameList.Add(item.Username);
+                        paswordList.Add(item.Pasword);
+                        jmbgList.Add(item.JMBG);
+                    }
+
+                    if (!usernameList.Contains(username) && !paswordList.Contains(pasword) && !jmbgList.Contains(jmbg))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                return false;
+            }
+        }
+
+    }
+}
+
---
+++ Nedeljni_I_Boris_Prpos/Zadatak_1/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using Zadatak_1.Command;
+using Zadatak_1.View;
+
+namespace Zadatak_1.ViewModel
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        MainWindow main;
+        public MainWindowViewModel(MainWindow mainOpen)
+        {
+            main = mainOpen;
+        }
+
+       #region Properties
+        private string username;
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                username = value;
+                OnPropertyChanged("Username");
+            }
+        }
+
+        private string password;
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+
+        #endregion
+
+        #region Commands
+        private ICommand close;
+        public ICommand Close
+        {
+            get
+            {
+                if (close == null)
+                {
+                    close = new RelayCommand(param => CloseExecute(), param => CanCloseExecute());
+                }
+                return close;
+            }
+        }
+        private void CloseExecute()
+        {
+            main.Close();
+        }
+        private bool CanCloseExecute()
+        {
+            return true;
+        }
+
+        private ICommand login;
+        public ICommand Login
+        {
+            get
+            {
+                if (login == null)
+                {
+                    login = new RelayCommand(param => LoginExecute(), param => CanLoginExecute());
+                }
+                return login;
+            }
+        }
+        private void LoginExecute()
+        {
+
+
+            try
+            {
+                //Master logged in
+                if (Username=="WPFMaster" && Password=="WPFAccess")
+                {
+                    Master master = new Master();
+                    master.ShowDialog();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                throw;
+            }
+        }
+        private bool CanLoginExecute()
+        {
+            if (String.IsNullOrEmpty(Username) || String.IsNullOrEmpty(Password))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        #endregion
+    }
+}
